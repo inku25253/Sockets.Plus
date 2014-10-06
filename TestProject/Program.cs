5e9a4efcae1f982777fd6a54856efb41a7010bd8
inku25253 <inku@potechi.org>
2014-10-06 18:46:35 +0900

    Test
    テスト

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -12,7 +13,8 @@ using System.Threading.Tasks;
 
 namespace TestProject
 {
-	class Program
+	[TestClass]
+	public class Program
 	{
 		static Stopwatch sw;
 		static TimeSpan sendTest = new TimeSpan(0);
@@ -21,96 +23,102 @@ namespace TestProject
 		static TimeSpan oneMilliseconds = new TimeSpan(0, 0, 0, 0, 1);
 		static void Main(string[] args)
 		{
+			/*
+				TimeSpan serverStart = new TimeSpan(0), clientStart = new TimeSpan(0);
 
-			TimeSpan serverStart = new TimeSpan(0), clientStart = new TimeSpan(0);
+				Random rand = new Random();
 
-			Random rand = new Random();
+				sw = new Stopwatch();
 
-			sw = new Stopwatch();
+				while (count < 10000)
+				{
+					done.Reset();
 
-			while (count < 10000)
-			{
-				done.Reset();
+					Console.SetCursorPosition(0, 0);
+					sw.Restart();
 
-				Console.SetCursorPosition(0, 0);
-				sw.Restart();
+					SocketServer server = new SocketServer();
 
-				SocketServer server = new SocketServer();
-
-				server.Throwable_Setup(new IPEndPoint(IPAddress.Any, 990));
+					server.Throwable_Setup(new IPEndPoint(IPAddress.Any, 990));
 
 
-				server.OnConnectRequest +=server_OnConnectRequest;
-				server.OnDataReceived +=server_OnDataReceived;
-				server.OnDisconnect +=server_OnDisconnect;
-				server.Start();
-				sw.Stop();
-				//Console.WriteLine("Server StartTime: {0}", sw.Elapsed);
-				if (sw.Elapsed < oneMilliseconds)
-					++oServerStart;
-				serverStart += sw.Elapsed;
-				sw.Restart();
+					server.OnConnectRequest +=server_OnConnectRequest;
+					server.OnDataReceived +=server_OnDataReceived;
+					server.OnDisconnect +=server_OnDisconnect;
+					server.Start();
+					sw.Stop();
+					//Console.WriteLine("Server StartTime: {0}", sw.Elapsed);
+					if (sw.Elapsed < oneMilliseconds)
+						++oServerStart;
+					serverStart += sw.Elapsed;
+					sw.Restart();
 
-				SocketClient client = new SocketClient();
-				client.OnConnected +=client_OnConnected;
-				client.OnDataReceived += client_OnDataReceived;
-				client.OnDisconnect +=client_OnDisconnect;
-				client.Connect("127.0.0.1", 990);
+					SocketClient client = new SocketClient();
+					client.OnConnected +=client_OnConnected;
+					client.OnDataReceived += client_OnDataReceived;
+					client.OnDisconnect +=client_OnDisconnect;
+					client.Connect("127.0.0.1", 990);
 
-				sw.Stop();
-				//Console.WriteLine("Client StartTime: {0}", sw.Elapsed);
-				if (sw.Elapsed < oneMilliseconds)
-					++oClientStart;
-				clientStart += sw.Elapsed;
+					sw.Stop();
+					//Console.WriteLine("Client StartTime: {0}", sw.Elapsed);
+					if (sw.Elapsed < oneMilliseconds)
+						++oClientStart;
+					clientStart += sw.Elapsed;
 
-				sw.Restart();
-				byte[] sendData = new byte[1024];
-				rand.NextBytes(sendData);
-				client.Send(sendData);
+					sw.Restart();
+					byte[] sendData = new byte[1024];
+					rand.NextBytes(sendData);
+					client.Send(sendData);
 
-				//Console.ReadLine();
-				done.WaitOne();
+					//Console.ReadLine();
+					done.WaitOne();
 
-				++count;
-				TimeSpan current;
-				/*
-				Console.WriteLine("=====平均=====                                               ");
+					++count;
+					TimeSpan current;
+				
+					Console.WriteLine("=====平均=====                                               ");
 
-				Console.WriteLine("ServerStart: {0}", new TimeSpan(serverStart.Ticks / count));
-				Console.WriteLine("ClientStart: {0}", new TimeSpan(clientStart.Ticks / count));
-				Console.WriteLine("SendTest   : {0}", new TimeSpan(sendTest.Ticks / count));
-				Console.WriteLine("=====平均=====                                               ");
-				Console.WriteLine();
-				Console.WriteLine("Count: {0:000}", count);
-				Console.WriteLine();
-				Console.WriteLine("=====1ms以上かかった回数=====                                ");
-				Console.WriteLine("ServerStart: {0}", oServerStart);
-				Console.WriteLine("ClientStart: {0}", oClientStart);
-				Console.WriteLine("SendTest   : {0}", oSend);
-				Console.WriteLine("=====1ms以上かかった回数=====                                ");*/
-				client.Close();
-				server.Stop();
+					Console.WriteLine("ServerStart: {0}", new TimeSpan(serverStart.Ticks / count));
+					Console.WriteLine("ClientStart: {0}", new TimeSpan(clientStart.Ticks / count));
+					Console.WriteLine("SendTest   : {0}", new TimeSpan(sendTest.Ticks / count));
+					Console.WriteLine("=====平均=====                                               ");
+					Console.WriteLine();
+					Console.WriteLine("Count: {0:000}", count);
+					Console.WriteLine();
+					Console.WriteLine("=====1ms以上かかった回数=====                                ");
+					Console.WriteLine("ServerStart: {0}", oServerStart);
+					Console.WriteLine("ClientStart: {0}", oClientStart);
+					Console.WriteLine("SendTest   : {0}", oSend);
+					Console.WriteLine("=====1ms以上かかった回数=====                                ");
+					client.Close();
+					server.Stop();
 
-				//Thread.Sleep(100);
-			}
+					//Thread.Sleep(100);
+				}
 
-			StreamWriter writer = new StreamWriter("log.log");
-			writer.WriteLine("=====平均=====                                               ");
+				StreamWriter writer = new StreamWriter("log.log");
+				writer.WriteLine("=====平均=====                                               ");
 
-			writer.WriteLine("ServerStart: {0}", new TimeSpan(serverStart.Ticks / count));
-			writer.WriteLine("ClientStart: {0}", new TimeSpan(clientStart.Ticks / count));
-			writer.WriteLine("SendTest   : {0}", new TimeSpan(sendTest.Ticks / count));
-			writer.WriteLine("=====平均=====                                               ");
-			writer.WriteLine();
-			writer.WriteLine("=====1ms以上かかった回数=====                                ");
-			writer.WriteLine("ServerStart: {0}({1:p})", oServerStart, (oServerStart / 10000) * 100);
-			writer.WriteLine("ClientStart: {0}({1:p})", oClientStart, (oClientStart / 10000) * 100);
-			writer.WriteLine("SendTest   : {0}({1:p})", oSend, (oSend / 10000) * 100);
-			writer.WriteLine("=====1ms以上かかった回数=====                                ");
-			writer.Flush();
-			writer.Close();
-			writer.Dispose();
+				writer.WriteLine("ServerStart: {0}", new TimeSpan(serverStart.Ticks / count));
+				writer.WriteLine("ClientStart: {0}", new TimeSpan(clientStart.Ticks / count));
+				writer.WriteLine("SendTest   : {0}", new TimeSpan(sendTest.Ticks / count));
+				writer.WriteLine("=====平均=====                                               ");
+				writer.WriteLine();
+				writer.WriteLine("=====1ms以上かかった回数=====                                ");
+				writer.WriteLine("ServerStart: {0}({1:p})", oServerStart, (oServerStart / 10000) * 100);
+				writer.WriteLine("ClientStart: {0}({1:p})", oClientStart, (oClientStart / 10000) * 100);
+				writer.WriteLine("SendTest   : {0}({1:p})", oSend, (oSend / 10000) * 100);
+				writer.WriteLine("=====1ms以上かかった回数=====                                ");
+				writer.Flush();
+				writer.Close();
+				writer.Dispose();*/
+
+
+
+
+
 		}
+
 
 		private static void client_OnDisconnect(object sender, SocketEventArgs<object, BytePacket, BytePacket> args)
 		{
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Sockets.Plus;
 
 namespace UnitTestProject1
 {
 	[TestClass]
 	public class UnitTest1
 	{
+
 		[TestMethod]
-		public void TestMethod1()
+		public void ポート使用可能テスト()
 		{
+			Assert.IsTrue(SocketServer.CheckPort(980));
+		}
+
+		[TestMethod]
+		public void ポート使用不可テスト()
+		{
+
+			SocketServer server = new SocketServer();
+			server.Setup(980);
+			server.Start();
+
+			Assert.IsFalse(SocketServer.CheckPort(980));
+
+
 		}
 	}
 }
